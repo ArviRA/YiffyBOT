eff28c5b1af9841d6a00689aa8a848f3fde6e477
unknown <arvindhak47@gmail.com>
2020-07-16 10:41:36 +0530

    removed bugs

--- scrap.py
+++ scrap.py
@@ -4,6 +4,12 @@ print("https://yts.mx/browse-movies/{}/all/all/0/latest/0/all".format('Iron'))
 all_links = requests.get("https://yts.mx/browse-movies/{}/all/all/0/latest/0/all".format('Iron'))
 page = BeautifulSoup(all_links.content, 'html.parser')
 mydivs = page.findAll("a", {"class":"browse-movie-title"})
+search_result = []
 for i in mydivs:
     dummy=i.text
-    print(dummy)
+    if "[" in dummy :
+        dummy = dummy.split("] ")[1]
+   
+    search_result.append(dummy)
+print(search_result)
+movie = requests.get("https://yts.mx/movies/{}".format(search_result[1]))
