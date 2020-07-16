6b1c0e4ef16ce956b09aca6ed772ee59f63a7575
unknown <arvindhak47@gmail.com>
2020-07-16 21:02:38 +0530

    hello

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:python3 app.py
+web: gunicorn --bind 0.0.0.0:${PORT} wsgi
--- app.py
+++ app.py
@@ -147,4 +147,5 @@ def webhook():
     return '!',200
 #bot.polling()
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+     port = int(os.environ.get('PORT', 5000))
+     app.run(host='0.0.0.0', port=port)
