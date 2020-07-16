39b4a9b3f6452d674094e43bb34df48acf79374c
unknown <arvindhak47@gmail.com>
2020-07-16 19:56:04 +0530

    uploading v1.0 of telebot

--- telegram.py
+++ telegram.py
@@ -147,4 +147,4 @@ def webhook():
     return '!',200
 #bot.polling()
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1',port = int(os.environ.get('PORT',5000)))
+    app.run(host = '0.0.0.0',port = int(os.environ.get('PORT',5000)))
