b2e0df20163713fdd24bb30355e33710710f8002
unknown <arvindhak47@gmail.com>
2020-07-17 12:53:39 +0530

    trials

--- try.py
+++ try.py
@@ -18,7 +18,6 @@ def sendMessage(message, text):
 # This method will send a message formatted in HTML to the user whenever it starts the bot with the /start command, feel free to add as many commands' handlers as you want
 @bot.message_handler(commands=['start'])
 def send_info(message):
-   global search
    text = (
    "<b>Welcome to the Yify BOt 🤖!</b>\n"
    "Say /help to the bot to get to know about it!"
@@ -39,6 +38,7 @@ def send_info(message):
    bot.send_message(message.chat.id, text, parse_mode='HTML')
 @bot.message_handler(commands=['search'])
 def send_info(message):
+   global search
    text = (
    "Enter the movie name to search"
    )
@@ -52,8 +52,10 @@ def reply_to_message(message):
    if 'hello'in message.text.lower():
       sendMessage(message, 'Hello! How are you doing today?')
    else:
+      print(search)
       if(search):
         base_url = ""
+        
         #if back == False:
         current_message = message
         print("\n\n\n message ",message.text,"\n\n\n")
